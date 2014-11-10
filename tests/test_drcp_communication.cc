744b4842ff91094933f3ea70fe04e73f5bdf1716
Robert Tiemann <r.tiemann@ta-hifi.com>
2014-11-10 10:52:17 +0100

    More unit tests for DRCPD communication code.

--- tests/test_drcp_communication.cc
+++ tests/test_drcp_communication.cc
@@ -63,6 +63,9 @@ void cut_teardown(void)
 }
 
 
+/*!
+ * Local mock implementation of #fifo_try_read_to_buffer().
+ */
 static int fill_buffer(uint8_t *dest, size_t count, size_t *add_bytes_read,
                        int fd)
 {
@@ -78,6 +81,9 @@ static int fill_buffer(uint8_t *dest, size_t count, size_t *add_bytes_read,
     return fill_buffer_data.return_value;
 }
 
+/*!\test
+ * Reading of a valid size header works.
+ */
 void test_read_drcp_size_header(void)
 {
     dynamic_buffer_check_space(&buffer);
@@ -92,6 +98,192 @@ void test_read_drcp_size_header(void)
     cut_assert_true(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
     cppcut_assert_equal(size_t(731), size);
     cppcut_assert_equal(sizeof(input_string) - 1, offset);
+}
+
+/*!\test
+ * Attempting to read size header from empty input fails.
+ */
+void test_read_drcp_size_header_from_empty_input(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    fill_buffer_data.data = "";
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error(EINVAL, LOG_CRIT, "Too short input, expected XML size");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header from half-ready input fails.
+ *
+ * This should not happen in practice since we are operating on named pipes
+ * which offer certain atomicity of reads and writes. Partial reads and writes
+ * should therefore not be a problem, but we handle the theoretically
+ * impossible case anyway and document the expected behavior in form of this
+ * test.
+ */
+void test_read_drcp_size_header_from_nearly_empty_input(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error(EINVAL, LOG_CRIT, "Too short input, expected XML size");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header not terminated with a newline character.
+ *
+ * To avoid misinterpretation of half-received buffers, any size header must be
+ * terminated with a newline character.
+ *
+ * This should not happen in practice since we are operating on named pipes
+ * which offer certain atomicity of reads and writes. Partial reads and writes
+ * should therefore not be a problem, but we handle the theoretically
+ * impossible case anyway and document the expected behavior in form of this
+ * test.
+ */
+void test_read_drcp_size_header_from_incomplete_input(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size: 5";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error(EINVAL, LOG_CRIT, "Incomplete XML size");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header with a number followed by some non-digit.
+ */
+void test_read_drcp_size_header_with_trailing_byte(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size: 123F\n";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error_formatted(EINVAL, LOG_CRIT, "Malformed XML size \"123F\" (Invalid argument)");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header containing negative size.
+ */
+void test_read_drcp_size_header_with_negative_size(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size: -5\n";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error_formatted(ERANGE, LOG_CRIT, "Too large XML size -5 (Numerical result out of range)");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header containing an unreasonably big size.
+ */
+void test_read_drcp_size_header_with_huge_size(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size: 65536\n";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error_formatted(ERANGE, LOG_CRIT, "Too large XML size 65536 (Numerical result out of range)");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header causing an integer overflow.
+ */
+void test_read_drcp_size_header_with_overflow_size(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "Size: 18446744073709551616\n";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 0;
+
+    mock_messages->expect_msg_error_formatted(ERANGE, LOG_CRIT, "Too large XML size 18446744073709551616 (Numerical result out of range)");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
+}
+
+/*!\test
+ * Attempting to read size header from unrelated data fails.
+ *
+ * This may happen if the sender and receiver fall out of sync, maybe due to
+ * some bug that causes the size header to be incorrect for some transaction.
+ * Resynchronization is not handled here.
+ */
+void test_read_faulty_drcp_size_header(void)
+{
+    dynamic_buffer_check_space(&buffer);
+    mock_named_pipe->expect_fifo_try_read_to_buffer_callback(fill_buffer);
+
+    static const char input_string[] = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    fill_buffer_data.data = input_string;
+    fill_buffer_data.return_value = 1;
+
+    mock_messages->expect_msg_error(EINVAL, LOG_CRIT, "Invalid input, expected XML size");
+
+    size_t size = 500;
+    size_t offset = 600;
+    cut_assert_false(drcp_read_size_from_fd(&buffer, &fds, &size, &offset));
+    cppcut_assert_equal(size_t(500), size);
+    cppcut_assert_equal(size_t(600), offset);
 }
 
 };
