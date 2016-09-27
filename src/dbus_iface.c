a67b4ffe7b18dd8aefcb0d978c75bacaa1574eca
Robert Tiemann <r.tiemann@ta-hifi.com>
2016-09-27 14:59:52 +0200

    Add function for creating a net.connman.Service proxy.
    Change-Id: I4a06162ced406445667c4f9fc696b0c58c00c525

--- src/dbus_iface.c
+++ src/dbus_iface.c
@@ -612,6 +612,23 @@ tdbusconnmanTechnology *dbus_get_connman_technology_proxy_for_object_path(const 
     return proxy;
 }
 
+tdbusconnmanService *dbus_get_connman_service_proxy_for_object_path(const char *path)
+{
+    GDBusConnection *connection =
+        g_dbus_proxy_get_connection(G_DBUS_PROXY(dbus_get_connman_manager_iface()));
+
+    GError *error = NULL;
+
+    tdbusconnmanService *proxy =
+        tdbus_connman_service_proxy_new_sync(connection,
+                                             G_DBUS_PROXY_FLAGS_NONE,
+                                             "net.connman", strdup(path),
+                                             NULL, &error);
+    (void)dbus_common_handle_dbus_error(&error);
+
+    return proxy;
+}
+
 tdbuslogindManager *dbus_get_logind_manager_iface(void)
 {
     return login1_iface_data.login1_manager_iface;
