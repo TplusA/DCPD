c441a27d1ccec4d636bf78a454a9c2975b1a6b2b
Robert Tiemann <r.tiemann@ta-hifi.com>
2018-11-19 08:40:39 +0100

    Connman: Monitor technology powered state, keep powered.
    This is a far less intrusive way to keep up power than blindly trying
    to enable powered state on each status change report received from
    Connman.
    
    Change-Id: I86bda00b869688d81f8e42cfa61bdfe127abc95b

--- src/dcpd.cc
+++ src/dcpd.cc
@@ -1466,6 +1466,28 @@ int main(int argc, char *argv[])
     }
 
     Connman::TechnologyRegistry::late_init();
+    Connman::TechnologyRegistry::get_singleton_for_update().first.register_property_watcher(
+        [] (Connman::TechnologyPropertiesWIFI::Property property,
+            Connman::TechnologyPropertiesBase::StoreResult result,
+            Connman::TechnologyPropertiesWIFI &props)
+        {
+            switch(property)
+            {
+              case Connman::TechnologyPropertiesWIFI::Property::POWERED:
+                if(!props.get<Connman::TechnologyPropertiesWIFI::Property::POWERED>())
+                    Connman::wlan_power_on();
+                break;
+
+              case Connman::TechnologyPropertiesWIFI::Property::CONNECTED:
+              case Connman::TechnologyPropertiesWIFI::Property::NAME:
+              case Connman::TechnologyPropertiesWIFI::Property::TYPE:
+              case Connman::TechnologyPropertiesWIFI::Property::TETHERING:
+              case Connman::TechnologyPropertiesWIFI::Property::TETHERING_IDENTIFIER:
+              case Connman::TechnologyPropertiesWIFI::Property::TETHERING_PASSPHRASE:
+                break;
+            }
+        });
+
     Regs::PlayStream::late_init();
 
     static struct sigaction action;
