9340568a69b0a89eb57d90afa6f072a613c66bb4
Robert Tiemann <r.tiemann@ta-hifi.com>
2021-09-15 13:33:15 +0200

    scripts: For some, allow passing a destination host as option.
    Change-Id: I445321f1e267a783f41feedb90e8eda4f505cde9

--- scripts/JumpToPos.sh
+++ scripts/JumpToPos.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+HOST='r1000e'
+
 if test $# -eq 0
 then
     MS=0
@@ -11,4 +13,4 @@ fi
 
 MS_HEX="$(printf "%08x" $((MS * 1000)) | sed 's/\(..\)\(..\)\(..\)\(..\)/0x\4 0x\3 0x\2 0x\1/g')"
 
-exec $(dirname $0)/send_dcp_command.sh -- w 73 0xc4 ${MS_HEX}
+exec $(dirname $0)/send_dcp_command.sh ${HOST} -- w 73 0xc4 ${MS_HEX}
