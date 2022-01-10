14d30a15c8db025296f0a1d6a1d41b0ac1edea49
Robert Tiemann <r.tiemann@ta-hifi.com>
2022-01-10 13:14:01 +0100

    play_app_stream: Support registers 78/79 and 238/239.
    Change-Id: Ifcf58e03c31463cd9ab583f955c829c91229c8f4

--- scripts/play_app_stream.sh
+++ scripts/play_app_stream.sh
@@ -11,9 +11,27 @@ string_to_hex()
     done
 }
 
+if test $# -ge 1 && test "x$1" = "x-n"
+then
+    REGISTER_METADATA=238
+    REGISTER_URL=239
+    shift
+else
+    REGISTER_METADATA=78
+    REGISTER_URL=79
+fi
+
 if test $# -ne 1 && test $# -ne 4
 then
-    echo "Usage: $0 <url> [<artist> <album> <title>]"
+    cat <<EOF
+Usage: $0 [-n] <url> [<artist> <album> <title>]
+
+-n      Push as next stream, not first stream.
+url     The URL to play.
+artist  Artist name.
+album   Album name.
+title   Title of the stream.
+EOF
     exit 1
 fi
 
@@ -38,5 +56,5 @@ METADATA="${METADATA}0x1d ${HEXSTRING}"
 string_to_hex "${TITLE}"
 METADATA="${METADATA}0x1d ${HEXSTRING}"
 
-$(dirname $0)/send_dcp_command.sh ${ADDR} -- w 78 ${METADATA}
-exec $(dirname $0)/send_dcp_command.sh ${ADDR} -- w 79 ${URL}
+$(dirname $0)/send_dcp_command.sh ${ADDR} -- w ${REGISTER_METADATA} ${METADATA}
+exec $(dirname $0)/send_dcp_command.sh ${ADDR} -- w ${REGISTER_URL} ${URL}
