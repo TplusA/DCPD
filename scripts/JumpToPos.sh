db859ec996f47dd8839e17540ae2506bc9aa79c3
Robert Tiemann <r.tiemann@ta-hifi.com>
2022-08-16 13:07:07 +0200

    scripts: Allow passing a host to JumpToPos.sh.
    Change-Id: I6af6e4a4a17609bb3fbf7971363fa77f8c047d23

--- scripts/JumpToPos.sh
+++ scripts/JumpToPos.sh
@@ -2,13 +2,19 @@
 
 set -eu
 
-HOST='r1000e'
+DEFAULT_HOST='r1000e'
 
 if test $# -eq 0
 then
     MS=0
-else
+    HOST="${DEFAULT_HOST}"
+elif test $# -eq 1
+then
     MS="$1"
+    HOST="${DEFAULT_HOST}"
+else
+    MS="$2"
+    HOST="$1"
 fi
 
 MS_HEX="$(printf "%08x" $((MS * 1000)) | sed 's/\(..\)\(..\)\(..\)\(..\)/0x\4 0x\3 0x\2 0x\1/g')"
